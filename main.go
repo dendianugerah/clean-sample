c8b5a17ffc09cf94de8d8a9f7b6cc14140a31e5b
Dendi <dendiaa@students.amikom.ac.id>
2022-11-28 22:13:02 +0700

    feat: main route

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"ddd-sample/config"
+	infra "ddd-sample/infra"
+	"ddd-sample/interface/handler"
+	"ddd-sample/interface/route"
+	"ddd-sample/usecase"
 
+	_ "ddd-sample/docs"
+
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+func main() {
+	e := echo.New()
+	db := config.NewDB()
+
+	//user
+	userRepository := infra.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userHandler := handler.NewUserHandler(userUsecase)
+	route.InitUserRoute(e, userHandler)
+
+	//task
+	taskRepository := infra.NewTaskRepository(db)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskHandler := handler.NewTaskHandler(taskUsecase)
+	route.InitTaskRoute(e, taskHandler)
+
+	//swagger
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	e.Logger.Fatal(e.Start(":8000"))
 }
